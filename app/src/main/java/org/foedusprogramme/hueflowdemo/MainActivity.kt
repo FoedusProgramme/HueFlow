54aaa22ed22367e0c75fbcc0d59fef88d052f338
nift4 <nift4@protonmail.com>
2025-09-10 11:40:10 +0200

    remove dead code, break red hue button

--- app/src/main/java/org/foedusprogramme/hueflowdemo/MainActivity.kt
+++ app/src/main/java/org/foedusprogramme/hueflowdemo/MainActivity.kt
@@ -1,17 +1,12 @@
 package org.foedusprogramme.hueflowdemo
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import org.foedusprogramme.hueflow.core.DynamicColorContextWrapper
 
 class MainActivity : AppCompatActivity() {
-    override fun attachBaseContext(newBase: Context) {
-        super.attachBaseContext(DynamicColorContextWrapper(newBase))
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
